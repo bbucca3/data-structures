b3bfdb4c15c6ed9fbae61874155504d530507430
Hack Reactor Student <student@hackreactor.com>
2017-05-17 16:33:45 -0700

    Part 1 complete - The Functional Pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,31 @@
 var Queue = function() {
   var someInstance = {};
-
+  
+  let size = 0;
+  let next = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    var remove = storage[next];
+    next++;
+    //size--;
+    return remove;
   };
 
   someInstance.size = function() {
+    var totalSize = size - next;
+    if (totalSize < 0) {
+      return 0;
+    }
+    return totalSize;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    let remove = storage[size - 1];
+    delete remove;
+    size--;
+    if (size < 0) { 
+      size = 0;
+    }
+    return remove;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
