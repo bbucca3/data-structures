2ddab74f951c2223c6091da6b7f147363a2a21c6
Hack Reactor Student <student@hackreactor.com>
2017-05-18 20:50:50 -0700

    Completed LinkedLists.  Working on tree contains.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-  };
+    list[value] = new Node(value);
+    let newNode = list[value];
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode.value;
+      list.tail = newNode;
+    }
+  };  
 
   list.removeHead = function() {
+    if (list.head) {
+      let formerHead = list.head;
+      list.head = list[list.head.next];
+      return formerHead.value;
+    }
   };
 
   list.contains = function(target) {
+    if (list.head.value === target) {
+      return true;
+    } else if (list.head.next !== null) {
+      list.head = list[list.head.next];
+      return list.contains(target);
+    }
+    return false;
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods); // was an object {}
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // was null // fix me //Object.create(treeMethods);
 
   return newTree;
 };
@@ -11,9 +11,24 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value); // child is an instantiation of Tree class
+  this.children.push(child); // pushing that created child obj to children array of tree
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for (var index = 0; index < this.children.length; index++) {
+    
+    var child = this.children[index];
+    treeMethods.contains.call(child, target);
+  }
+  return false;
+// A .contains() method, takes any input and
+// returns a boolean reflecting whether it can be found as the value
+// of the target node or any descendant node
+  
 };
 
 
@@ -21,3 +36,12 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ 
+  //  if (this.value === target) {
+  //   return true;
+  // }
+  // for (let index = 0; index < this.children.length; index++) {
+  //   let child = this.children[index];
+  //   return treeMethods.contains.call(child, target);
+  // }
+  // return false;
