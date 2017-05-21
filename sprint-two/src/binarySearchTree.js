82a3ece87a42becbb6324f1947c3af8239123ef3
Hack Reactor Student <student@hackreactor.com>
2017-05-20 21:52:59 -0700

    WE DID IT!!!!!!!!!!!!!!

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,7 @@
 var BinarySearchTree = function(value) {
   var binarySearchTree = Object.create(binaryTreeMethods);
-  binarySearchTree.right = {};
-  binarySearchTree.left = {};
+  binarySearchTree.right;
+  binarySearchTree.left;
   binarySearchTree.value = value;
   return binarySearchTree;
 };
@@ -9,15 +9,54 @@ var BinarySearchTree = function(value) {
 var binaryTreeMethods = {};
 
 binaryTreeMethods.insert = function(value) {
+  // check value for greater than or less than root
+  if (value < this.value) {
+    // if left node does exist, recurse
+    if (this.left) {
+      this.left.insert(value);
+    // if left node does not exist, then create new
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  }
   if (value > this.value) {
-    this.right.value = BinarySearchTree(value); 
-  } else {
-    this.left.value = BinarySearchTree(value);
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
   }
 };
 
-binaryTreeMethods.contains = function(value) {};
-binaryTreeMethods.depthFirstLog = function(value) {};
+binaryTreeMethods.contains = function(value) {
+  // check if value equals target value
+  if (this.value === value) {
+    return true;
+  }
+  if (this.right === undefined || this.left === undefined) {
+    return false;
+  }
+  if (value < this.value) {
+    return this.left.contains(value);
+  }
+  if (value > this.value) {
+    return this.right.contains(value);
+  }
+  return false;
+};
+
+binaryTreeMethods.depthFirstLog = function(callback) {
+  // callback on first node
+  callback(this.value);
+  // check left then recurse
+  if (this.left !== undefined) {
+    this.left.depthFirstLog(callback);
+  }
+  // check right then recurse
+  if (this.right !== undefined) {
+    this.right.depthFirstLog(callback);
+  }
+};
 
 
 
